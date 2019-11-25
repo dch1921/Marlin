85d5b9d31c2eb75689878ad0bcea3d1dd709fd18
dch <na>
2019-11-24 21:00:07 -0600

    Update to z-axis autoalight position

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -606,7 +606,7 @@
 #define Z_STEPPER_AUTO_ALIGN
 #if ENABLED(Z_STEPPER_AUTO_ALIGN)
   // Define probe X and Y positions for Z1, Z2 [, Z3]
-  #define Z_STEPPER_ALIGN_XY { {  30, 160 }, { 270,  160 } }//, { 290, 290 } }
+  #define Z_STEPPER_ALIGN_XY { {  40, 160 }, { 260,  160 } }//, { 290, 290 } }
 
   // Provide Z stepper positions for more rapid convergence in bed alignment.
   // Currently requires triple stepper drivers.
